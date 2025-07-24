84b1d1ea3108ee1bc9cf7a4d530d35a862c51bff
César Gaiotto <cesaraugusto.gaiotto@gmail.com>
2025-07-24 17:29:53 -0300

    first commit

---
+++ exercicios/gameAdivinhacao/src/testeVagaJrGPT/Main.java
@@ -0,0 +1,32 @@
+package testeVagaJrGPT;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int num;
+		int soma = 0;
+		
+		System.out.print("Digite um numero:");
+		num = scan.nextInt();
+		
+	if(num == 0 || num < 0) {
+		do {
+			System.out.print("\nO numéro deve ser maior que '0', tente novamente:");
+			num = scan.nextInt();
+		} while(num == 0 || num < 0);	
+	}
+	for(int i = 1; i < num; i++) {
+		if(i % 3 == 0 || i % 5 == 0) {
+		soma = soma + i;
+		
+		}
+	}	
+		
+		int resultado = soma;
+		System.out.println("\nResultado: "+ resultado);
+	}
+}
